57ceb97e4c8259ad0fa30b3b3874c4d7db7b362e
Stefany Guevara <efyguevara@gmail.com>
2019-08-02 15:50:42 -0400

    Updates

---
+++ precache-manifest.2fc25bf2e42294fdada609acba846046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377e72caa39f3622154cf4956021f7b5",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "e160a8b440e05ccd4335",
+    "url": "/Portafolio/static/css/main.fb56d6fd.chunk.css"
+  },
+  {
+    "revision": "246da6f25c5854697ef4",
+    "url": "/Portafolio/static/js/2.b896607b.chunk.js"
+  },
+  {
+    "revision": "e160a8b440e05ccd4335",
+    "url": "/Portafolio/static/js/main.fd0e5d21.chunk.js"
+  },
+  {
+    "revision": "0dceec934d17b8d03d71",
+    "url": "/Portafolio/static/js/runtime~main.9747bc56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portafolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
