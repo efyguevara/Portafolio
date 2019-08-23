1e94f38590130fcbada8db66d930013e1ffef9d8
BlancaCarrasco <blankcarr@gmail.com>
2019-08-23 18:18:52 -0400

    Add contact dates

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import logo from '../logo.svg';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Contact() {
     return (
         <>
-            <div>
+            <Container id="containerContact">
+                <Row>
+                    <div id="contactWidhtMe">
+                        <h2 className="pageTitle">Contacta conmigo</h2>
+                    </div>
+                </Row>
+                <Row>
+                    <Col>
+                        <div id="phoneNumber">
+                            <h3 className="">
+                                Número telefónico
+                            </h3>    
+                            <h5>
+                                (+56) 9 54906238
+                            </h5>
+                        </div>
+                        
+                        <div id="email"> 
+                            <h3 className="">
+                                Correo electrónico
+                            </h3>
+                            <h5 className="">
+                                stefanyguevarae@gmail.com
+                            </h5>
+                        </div>
 
-                <img src={logo} className="App-logo" alt="logo" />
-
-                <h1>CONTACT EN CONSTRUCCION</h1>
-
-            </div>
-
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div id="rrss">
+                            <h3>
+                                Redes sociales
+                            </h3>
+                        </div>  
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
